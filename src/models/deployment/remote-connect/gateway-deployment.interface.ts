a97c1abc3d2654f29ea922bf94441670dfd3873d
Matthew Mercado <matthew.mercado@salientsys.com>
2022-12-12 14:05:43 -0600

    adding optional variables

--- src/models/deployment/remote-connect/gateway-deployment.interface.ts
+++ src/models/deployment/remote-connect/gateway-deployment.interface.ts
@@ -2,6 +2,13 @@ interface IGatewayDeployment {
   deploymentName: string;
   customerName: string;
   customerTag: string;
+  // optional advanced variables
+  cloudProxyEcrTag?: string;
+  createPrivateLink?: string;
+  openVpnClientDhcpNetwork?: string;
+  openVpnClientDhcpNetworkMask?: string;
+  openVpnClientStaticNetwork?: string;
+  openVpnClientStaticNetworkMask?: string;
 }
 
 type IGatewayDeploymentDTO = IGatewayDeployment;
