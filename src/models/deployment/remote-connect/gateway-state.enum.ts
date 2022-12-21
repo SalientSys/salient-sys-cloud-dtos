30d43baacd78787be993330ee269330a87459a1e
Matthew Mercado <matthew.mercado@salientsys.com>
2022-12-21 14:59:34 -0600

    enum update

--- src/models/deployment/remote-connect/gateway-state.enum.ts
+++ src/models/deployment/remote-connect/gateway-state.enum.ts
@@ -2,4 +2,7 @@ export enum GatewayState {
   okay = 'okay',
   deploying = 'deploying',
   failed = 'failed',
+  vpnFailed = 'vpnFailed',
+  proxyFailed = 'proxyFailed',
+  secretManagerFailed = 'secretManagerFailed',
 }
