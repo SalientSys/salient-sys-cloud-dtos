c3ea6e0ee8db837e2237deb5b856ffac82026248
Ethan Flower <ethan.flower@salientsys.com>
2023-02-06 17:07:50 -0600

    added documentation

--- src/regexes/index.ts
+++ src/regexes/index.ts
@@ -1,24 +1,39 @@
-import { lastNameRegex } from '../..';
-
 const phoneNumberRegex = /^[-+0-9()]+$/;
 const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
-/*
-  This regular expression allows a maximum of two words separated by spaces, hyphens, or apostrophes.
-  The first and last characters must belong to any of the Unicode script ranges from Basic Latin to Latin Extended-A,
-  ensuring that the string starts and ends with a word character rather than a symbol.
-  The character range \u0041-\u005A corresponds to uppercase Latin letters (A-Z),
-   \u0061-\u007A to lowercase Latin letters (a-z), and \u00C0-\u017F to Latin-1 Supplement, which includes accented Latin letters.
-  The other character ranges \u0180-\u024F, \u0250-\u02AF, \u0370-\u03FF, \u0400-\u04FF, \u0500-\u052F,
-  and \u1E00-\u1EFF correspond to different Unicode scripts including Latin Extended-B, IPA Extensions, Greek, Cyrillic, and various other scripts.
+/**
+ * The `firstNameRegex` regular expression allows for a maximum of two words, separated by spaces, hyphens, or apostrophes.
+ * The first and last characters must be a letter character from any script or a character intended to be combined with others (e.g. accents).
+ * This allows for names written in any script and enables support for non-Latin characters.
+ *
+ * Example matches:
+ *   - "John"
+ *   - "Jane Doe"
+ *   - "José"
+ *   - "马修"
+ *
+ * Example non-matches:
+ *   - "John Doe!"
+ *   - "Jane Doe-"
+ *   - "José-Maria" (more than two words)
+ 
 */
 const firstNameRegex = /^[\p{L}\p{M}]+([ \-']{0,1}[\p{L}\p{M}]+){0,1}$/u;
 
-/*
-This regular expression allows a maximum of three words separated by spaces, hyphens, or apostrophes.
-The first and last characters must belong to any of the Unicode script ranges from Basic Latin to Latin Extended-A,
-ensuring that the string starts and ends with a word character rather than a symbol.
-The character ranges included in this regular expression are the same as in firstNameRegex.
-*/
+/**
+ * The `lastNameRegex` regular expression allows for a maximum of three words, separated by spaces, hyphens, or apostrophes.
+ * The first and last characters must be a letter character from any script or a character intended to be combined with others (e.g. accents).
+ * This allows for names written in any script and enables support for non-Latin characters.
+ *
+ * Example matches:
+ *   - "Smith"
+ *   - "De la Cruz"
+ *   - "王大力"
+ *
+ * Example non-matches:
+ *   - "De la Cruz!"
+ *   - "王大力-"
+ *   - "De la Cruz-Martinez" (more than three words)
+ */
 const lastNameRegex = /^[\p{L}\p{M}]+([ \-']{0,1}[\p{L}\p{M}]+){0,2}$/u;
 
 export { phoneNumberRegex, passwordRegex, lastNameRegex, firstNameRegex };
