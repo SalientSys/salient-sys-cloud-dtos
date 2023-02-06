d51ff6083b5ee5b35f693aaf1798b1a37ca46744
Dante Garcia <dante.garcia@salientsys.com>
2023-02-06 14:58:43 -0600

    only properties needed

--- src/dtos/recording-server/cameras/camera.model.ts
+++ src/dtos/recording-server/cameras/camera.model.ts
@@ -1,4 +1,4 @@
-import { Cone } from "./cone.model";
+import { ICone } from "./cone.model";
 import { PtzSetting } from "./ptz-settings.model";
 
 
@@ -10,57 +10,21 @@ interface ICameraEntity {
 }
 
 interface ICamera {
-  videoDevice: any | null;
-  advancedSetting: any | null;
-  edgeSetting: any | null;
-  recordingSetting: any | null;
-  camera360Lens: any | null;
-  ipSetting: any | null;
-  stretchSetting: any | null;
-  ptzSetting: PtzSetting | null;
-  streamProcessing: any | null;
-  cone: Cone | null;
-  nvrCamera: any | null;
-  cameraEvents: any | null;
-  motionZones: any | null;
-  mediaStreams: any | null;
-  cameraSchedules: any | null;
-  recordingServer: any | null;
-  volumes: any | null;
-  cameraId: number;
-  cameraGuid: string;
-  recordingServerId: number;
-  videoDeviceId: number;
-  isEnabled: boolean;
-  name: string;
-  timeZone: string;
-  cameraNo: number;
-  channelNo: number;
-  /** ENUM */
-  alarmPresetType: number;
-  motionSensitivity: number;
-  latitude: string | null;
-  longitude: string | null;
-  advancedSettingId: any | null;
-  edgeSettingId: number;
-  recordingSettingId: number;
-  camera360LensId: number;
-  ipSettingId: number;
-  stretchSettingId: number;
-  ptzSettingId: number;
-  streamProcessingId: number;
-  coneId: number;
-  note: any | null;
-  manufacturer: any | null;
-  model: any | null;
-  connectionType: any | null;
-  isAnalog: boolean;
-  isNetwork: boolean;
-  driverType: any | null;
-  serverGuid: string;
-  state: string;
-  ipStreamType: string;
-  cameraEntity: ICameraEntity;
+cameraId: string,
+cameraGuid: string,
+isEnabled: boolean,
+name: string,
+cone: ICone,
+channelNo: number,
+recordingServerId: string,
+latitude: string | null,
+longitude: string | null,
+timeZone: string,
+ptzSetting: PtzSetting | null,
+serverGuid: string,
+state: string;
+ipStreamType: string;
+cameraEntity: ICameraEntity;
 }
 
 export { ICameraEntity, ICamera }
