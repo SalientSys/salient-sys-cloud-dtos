0e3a4eedba562dbd88287d0be1ba8f4e7b2c60d9
Sohail Haqyar <sohail.haqyar@salientsys.com>
2023-02-08 14:57:41 -0600

    updated to better architecture

--- src/dtos/user/list-users.dto.ts
+++ src/dtos/user/list-users.dto.ts
@@ -1,4 +1,10 @@
+/**
+ * String booleans used in the query string validation.
+ */
+
+import { StringBoolean } from '../../misc';
+
 export interface IListUsersDTO {
-  isCommercial?: string;
-  includeSalient?: string;
+  isCommercial?: StringBoolean;
+  includeSalient?: StringBoolean;
 }
