d735d1992882e6dc2a9be59feb69044bec0ebeff
dante-salient <119631473+dante-salient@users.noreply.github.com>
2023-02-09 07:33:53 -0600

    addded camera models (#17)
    * addded camera models
    
    * ranaming types
    
    * only properties needed
    
    * added driver type
    
    * 1.0.50
    
    * 1.0.51
    
    ---------
    
    Co-authored-by: Dante Garcia <dangar@Dantes-MacBook-Pro-3.local>
    Co-authored-by: Dante Garcia <dangar@macbookpro-dangar-2015.local>
    Co-authored-by: Sohail Haqyar <sohail.haqyar@salientsys.com>

---
+++ src/dtos/recording-server/cameras/cone.model.ts
@@ -0,0 +1,10 @@
+export interface ICone {
+    isDeleted: boolean;
+    coneId: number;
+    /** Degrees */
+    rotateAngle: number;
+    /** Degrees */
+    widthAngle: number;
+    depth: number;
+  };
+  
